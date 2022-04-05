da41b51158600f6f8e0c4bdb1e2f46bd114fac40
Evan <ekprogamer13@gmail.com>
2022-04-04 22:02:57 -0400

    added service for windows

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ options = webdriver.ChromeOptions()
 # options.add_argument('--headless')
 # options.add_argument('--disable-gpu')
 options.add_argument('--disable-extensions')
-s = Service('/Users/evankolberg/VS Code Projects/GOAT.com Web Scraper/chromedriver')
+macOS_service = Service('/Users/evankolberg/VS Code Projects/GOAT.com Web Scraper/chromedriver')
+win_service = Service('C:\Users\ekpro\VS Code Projects\GOAT.com-Web-Scraper\chromedriver.exe')
 
-driver = webdriver.Chrome(service=s, options=options)
+driver = webdriver.Chrome(service=macOS_service, options=options)
 driver.set_window_size(2048, 1080)
 driver.set_window_position(1200, 200, windowHandle='current')
 
