56492bebbc0ea5c0a749f86f8b51e0cbdfca70c8
Evan <ekprogamer13@gmail.com>
2022-04-07 20:14:45 -0400

    almost fully working

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
-from itertools import product
 from selenium import webdriver
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
-from time import sleep
 
 options = webdriver.ChromeOptions()
 # options.add_argument('--headless')
-# options.add_argument('--disable-gpu')
 options.add_argument('--disable-extensions')
 macOS_service = Service('/Users/evankolberg/VS Code Projects/GOAT.com Web Scraper/chromedriver')
 win_service = Service('C:/Users/ekpro/VS Code Projects/GOAT.com-Web-Scraper/chromedriver.exe')
 
-driver = webdriver.Chrome(service=macOS_service, options=options)
+driver = webdriver.Chrome(service=win_service, options=options)
 driver.set_window_size(2048, 1080)
 driver.set_window_position(1200, 200, windowHandle='current')
+driver.minimize_window()
 
 
 
@@ -27,7 +22,6 @@ def inquery(query):
         if i % 2 == 1:
             query.insert(i, '%20')
     query = ''.join(query)
-    # ----------------------------------------
 
     driver.get(f'https://www.goat.com/search?query={query}')
 
@@ -40,24 +34,47 @@ def crawler():
     for i in soup.findAll('div', {'data-qa': 'grid_cell_product'}):
         link_queue.append(f"https://goat.com{i.find(href=True)['href']}")
 
-    # print(link_queue)
-
     for i in link_queue:
-        driver.get(i)
+        prices = []
+        counter = 0
 
+        driver.get(i)
         soup = BeautifulSoup(driver.page_source, 'html.parser')
 
+        # gets the name of the product
         product_info = soup.find('div', {'data-qa': 'product_year'}).get_text().replace(soup.find('ol').get_text(), '')
-        product_prices = soup.find('div', {'data-qa': 'buy_bar_desktop'}).get_text()
 
-        print('\n', product_info)
-        print(product_prices, '\n')
+        # for shoes, it iterates through all the available sizes
+        if soup.find('div', {'data-swiper-slide-index':0}):    # tests if it is infact shoes
+            while True:
+                if soup.find('div', {'data-swiper-slide-index':counter}):
+                    data = soup.find('div', {'data-swiper-slide-index':counter}).get_text()
+                    data = data[:data.find('$')] + ' ' + data[data.find('$'):]
+                    prices.append(data)
+                    counter += 1
+                else:
+                    break
+        else:
+            # for other things, NOT shoes, with the names of sizes
+            if len(prices) == 0:
+                try:
+                    for i in soup.findAll('div', {'data-qa': 'buy_bar_item_desktop'}):
+                        data = i.get_text()
+                        data = data[:data.find('$')] + ' ' + data[data.find('$'):]
+                        prices.append(data)
+                except AttributeError:
+                    prices.append('Currently Unavailable')
+
+        
+
+        print(f'\n{product_info}')
+        print(f'{prices}\n')
 
 
     
 
 
 if __name__ == '__main__':
-    inquery('air jordan pro')
+    inquery(input('Search for an item at https://goat.com:  '))
     crawler()
     driver.quit()
